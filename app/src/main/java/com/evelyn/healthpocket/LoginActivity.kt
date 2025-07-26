3be823edef5c79d0f96ba4a020c4aedae72230bf
Ngọc Lê <ngocle@Chi-ep-yang-ho-2.local>
2025-07-26 02:30:58 -0500

    Error HealthPocket keep stopping

---
+++ app/src/main/java/com/evelyn/healthpocket/LoginActivity.kt
@@ -0,0 +1,5 @@
+package com.evelyn.healthpocket
+
+class LoginActivity {
+
+}
---
+++ app/src/main/java/com/evelyn/healthpocket/SplashActivity.kt
@@ -0,0 +1,21 @@
+package com.evelyn.healthpocket
+
+import android.content.Intent // Use to switch between activities
+import android.content.SharedPreferences // Use to store key value (login status)
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+// Use AppCompatActivity for compatibility on old android versions
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // Check if the user is already logged in
+        val sharePref = getSharedPreferences("Username", MODE_PRIVATE)
+        val isLogin = sharePref.getBoolean("IsLogin", false) // retrieve login status, default false if not found
+
+
+        startActivity(Intent(this, DashBoardActivity::class.java))
+
+    }
+}
