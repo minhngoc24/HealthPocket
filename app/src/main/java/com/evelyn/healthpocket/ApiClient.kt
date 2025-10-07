3ca5fbcb85b982c57b99824b5d6311ef4b517e03
evelynnguyenh <nhdieulinh06@gmail.com>
2025-10-06 23:23:10 -0500

    Error fixed

--- app/src/main/java/com/evelyn/healthpocket/ApiClient.kt
+++ app/src/main/java/com/evelyn/healthpocket/ApiClient.kt
@@ -1,5 +1,8 @@
 package com.evelyn.healthpocket
 
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
 object ApiClient {
     private const val BASE_URL = "https://healthpocket-api-e5e8frc8cfhdhjby.westcentralus-01.azurewebsites.net/"
     val instance: Retrofit by lazy {
--- app/src/main/java/com/evelyn/healthpocket/MainActivity.kt
+++ app/src/main/java/com/evelyn/healthpocket/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.evelyn.healthpocket
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -12,16 +13,32 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.evelyn.healthpocket.ui.theme.HealthPocketTheme
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        // ✅ Gọi API khi app chạy
+        val api = ApiClient.instance.create(ApiService::class.java)
+        api.checkHealth().enqueue(object : Callback<Map<String, Boolean>> {
+            override fun onResponse(call: Call<Map<String, Boolean>>, response: Response<Map<String, Boolean>>) {
+                Log.d("API", "Response: ${response.body()}")
+            }
+
+            override fun onFailure(call: Call<Map<String, Boolean>>, t: Throwable) {
+                Log.e("API", "Error: ${t.message}")
+            }
+        })
+
         setContent {
             HealthPocketTheme {
-                Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "HealthPocket",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -32,10 +49,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Text(text = "Hello $name!", modifier = modifier)
 }
 
 @Preview(showBackground = true)
