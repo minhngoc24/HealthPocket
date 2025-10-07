46061a639bf75f219f1fdea10dd04c32730c18c4
evelynnguyenh <nhdieulinh06@gmail.com>
2025-10-07 00:23:15 -0500

    Error fixed

--- app/src/main/java/com/evelyn/healthpocket/ApiClient.kt
+++ app/src/main/java/com/evelyn/healthpocket/ApiClient.kt
@@ -1,14 +1,26 @@
 package com.evelyn.healthpocket
 
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object ApiClient {
     private const val BASE_URL = "https://healthpocket-api-e5e8frc8cfhdhjby.westcentralus-01.azurewebsites.net/"
-    val instance: Retrofit by lazy {
+    val instance: ApiService by lazy {
+        val logging = HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+
+        val client = OkHttpClient.Builder()
+            .addInterceptor(logging)
+            .build()
+
         Retrofit.Builder()
             .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
             .build()
+            .create(ApiService::class.java)
     }
 }
